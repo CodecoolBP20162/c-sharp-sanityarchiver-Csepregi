945aea73cb50a75ea3765a50b45d70750b43bd65
Gábor Csepregi <csepregigabor@outlook.com>
2017-06-28 15:18:02 +0200

    first commit, back

--- ConsoleApp1/SanityArchiver/Form1.Designer.cs
+++ ConsoleApp1/SanityArchiver/Form1.Designer.cs
@@ -29,11 +29,125 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.textPath = new System.Windows.Forms.ComboBox();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItemCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.back = new System.Windows.Forms.Button();
+            this.path = new System.Windows.Forms.Label();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.ImageIndex = 0;
+            this.treeView.ImageList = this.imageList1;
+            this.treeView.Location = new System.Drawing.Point(12, 93);
+            this.treeView.Name = "treeView";
+            this.treeView.SelectedImageIndex = 0;
+            this.treeView.Size = new System.Drawing.Size(744, 225);
+            this.treeView.TabIndex = 0;
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            this.treeView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.treeView_MouseDoubleClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "files.png");
+            this.imageList1.Images.SetKeyName(1, "folder.png");
+            // 
+            // textPath
+            // 
+            this.textPath.FormattingEnabled = true;
+            this.textPath.Location = new System.Drawing.Point(29, 21);
+            this.textPath.Name = "textPath";
+            this.textPath.Size = new System.Drawing.Size(121, 21);
+            this.textPath.TabIndex = 1;
+            this.textPath.SelectedIndexChanged += new System.EventHandler(this.textPath_SelectedIndexChanged);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemCopy,
+            this.toolStripMenuItemPaste,
+            this.toolStripMenuItemDelete});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(108, 70);
+            // 
+            // toolStripMenuItemCopy
+            // 
+            this.toolStripMenuItemCopy.Name = "toolStripMenuItemCopy";
+            this.toolStripMenuItemCopy.Size = new System.Drawing.Size(107, 22);
+            this.toolStripMenuItemCopy.Text = "Copy";
+            this.toolStripMenuItemCopy.Click += new System.EventHandler(this.toolStripMenuItemCopy_Click);
+            // 
+            // toolStripMenuItemPaste
+            // 
+            this.toolStripMenuItemPaste.Name = "toolStripMenuItemPaste";
+            this.toolStripMenuItemPaste.Size = new System.Drawing.Size(107, 22);
+            this.toolStripMenuItemPaste.Text = "Paste";
+            this.toolStripMenuItemPaste.Click += new System.EventHandler(this.toolStripMenuItemPaste_Click);
+            // 
+            // toolStripMenuItemDelete
+            // 
+            this.toolStripMenuItemDelete.Name = "toolStripMenuItemDelete";
+            this.toolStripMenuItemDelete.Size = new System.Drawing.Size(107, 22);
+            this.toolStripMenuItemDelete.Text = "Delete";
+            this.toolStripMenuItemDelete.Click += new System.EventHandler(this.toolStripMenuItemDelete_Click);
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(29, 64);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(75, 23);
+            this.back.TabIndex = 3;
+            this.back.Text = "<<";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // path
+            // 
+            this.path.AutoSize = true;
+            this.path.Location = new System.Drawing.Point(216, 24);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(32, 13);
+            this.path.TabIndex = 4;
+            this.path.Text = "Path:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 348);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.textPath);
+            this.Controls.Add(this.treeView);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ComboBox textPath;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemCopy;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemPaste;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemDelete;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Label path;
     }
 }
 
--- ConsoleApp1/SanityArchiver/Form1.cs
+++ ConsoleApp1/SanityArchiver/Form1.cs
@@ -7,14 +7,193 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace SanityArchiver
 {
     public partial class Form1 : Form
     {
+        public string currentDirectory;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            foreach (DriveInfo di in DriveInfo.GetDrives())
+            {
+                textPath.Items.Add(di);
+            }
+        }
+
+                  
+
+        private void textPath_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentDirectory = textPath.SelectedItem.ToString();
+            path.Text = currentDirectory;
+            treeView.Nodes.Clear();
+            try
+            {
+                DriveInfo drive = (DriveInfo)textPath.SelectedItem;
+                foreach (DirectoryInfo dirInfo in drive.RootDirectory.GetDirectories())
+                {
+           
+                    TreeNode node = new TreeNode();
+                    node.Text = dirInfo.Name;
+                    node.ImageIndex = 1;
+                    node.SelectedImageIndex = 1;
+                    treeView.Nodes.Add(node);                
+                }
+
+
+                foreach (FileInfo file in drive.RootDirectory.GetFiles())
+                {
+                    if (file.Exists)
+                    {
+                        TreeNode node = new TreeNode();
+                        node.Text = file.Name;
+                        node.ImageIndex = 0;
+                        node.SelectedImageIndex = 0;
+                        treeView.Nodes.Add(node.Text);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void treeView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            currentDirectory = currentDirectory +  treeView.SelectedNode.Text + "\\";
+            path.Text = currentDirectory;
+            try
+            {
+                DirectoryInfo drinfo = new DirectoryInfo(currentDirectory);
+                treeView.Nodes.Clear();
+                foreach (DirectoryInfo dr in drinfo.GetDirectories())
+                {
+                    Console.WriteLine(dr.Name);
+                    TreeNode node = new TreeNode();
+                    node.Text = dr.Name;
+                    node.ImageIndex = 1;
+                    node.SelectedImageIndex = 1;
+                    treeView.Nodes.Add(node);
+                }
+
+                foreach (FileInfo file in drinfo.GetFiles())
+                {
+                    if (file.Exists)
+                    {
+                        TreeNode node = new TreeNode();
+                        node.Text = file.Name;
+                        node.ImageIndex = 0;
+                        node.SelectedImageIndex = 0;
+                        treeView.Nodes.Add(node);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            //Console.WriteLine(e.Node.Text);
+        }
+
+        private void toolStripMenuItemCopy_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void toolStripMenuItemPaste_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItemDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo drinfo = new DirectoryInfo(textPath.Text);
+            int index = 0;
+            int occurance = 0;
+            for (int i = currentDirectory.Length-1; i > 0; i--)
+            {
+                if(currentDirectory[i] == '\\')
+                {
+                    occurance++;
+                }
+                if(occurance == 2)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            
+            if(drinfo != null)
+            {
+                currentDirectory = currentDirectory.Substring(0, index + 1);
+                path.Text = currentDirectory;
+
+                try
+                {
+                    
+                    treeView.Nodes.Clear();
+                    foreach (DirectoryInfo dr in drinfo.GetDirectories())
+                    {
+                        Console.WriteLine(dr.Name);
+                        TreeNode node = new TreeNode();
+                        node.Text = dr.Name;
+                        node.ImageIndex = 1;
+                        node.SelectedImageIndex = 1;
+                        treeView.Nodes.Add(node);
+                    }
+
+                    foreach (FileInfo file in drinfo.GetFiles())
+                    {
+                        if (file.Exists)
+                        {
+                            TreeNode node = new TreeNode();
+                            node.Text = file.Name;
+                            node.ImageIndex = 0;
+                            node.SelectedImageIndex = 0;
+                            treeView.Nodes.Add(node);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
     }
 }
+        
+    
+
